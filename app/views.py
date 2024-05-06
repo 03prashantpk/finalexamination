5003a189d0f24ec572090824223410229827501d
03prashantpk <prashantmanwan@gmail.com>
2024-05-06 11:37:56 +0530

    Form and bootstrap config

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from app.models import Contact
 
 # Create your views here.
 
 # index
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        contact = Contact(name=name, email=email, message=message)
+        contact.save()
+       
+        return JsonResponse({'message': 'Your message has been sent successfully!'})
+    
+    return render(request, 'contact.html')
--- finalexamination/urls.py
+++ finalexamination/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact/', views.contact, name='contact')
 ]
